2c767b6da0503378fdd1adcaf965b1803bbe85fb
AbelTarazona <abel.tarazona@urp.edu.pe>
2018-10-24 10:15:10 -0500

    se termino el layout de exercises asi como tambien la programacion

--- app/src/main/java/com/example/usuario/nikeapp/ExerciseScreen.java
+++ app/src/main/java/com/example/usuario/nikeapp/ExerciseScreen.java
@@ -1,12 +1,23 @@
 package com.example.usuario.nikeapp;
 
 import android.content.Context;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class ExerciseScreen extends AppCompatActivity {
+public class ExerciseScreen extends AppCompatActivity implements View.OnClickListener, GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+
+    LinearLayout nikeLayout;
+    Button btnClose;
+
+    GestureDetectorCompat gestureDetectorCompat;
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -17,5 +28,78 @@ public class ExerciseScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_screen);
+
+        nikeLayout = findViewById(R.id.nikeLayout);
+        btnClose = findViewById(R.id.btnClose); btnClose.setOnClickListener(this);
+
+        btnClose.setAlpha(0);
+
+        gestureDetectorCompat = new GestureDetectorCompat(this, this);
+        gestureDetectorCompat.setOnDoubleTapListener(this);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetectorCompat.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btnClose:
+                nikeLayout.animate().setDuration(800).translationY(1800).start();
+                btnClose.animate().alpha(0).setDuration(800);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        nikeLayout.animate().setDuration(800).translationY(0).start();
+        btnClose.animate().alpha(1).setDuration(800);
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
     }
 }
--- app/src/main/java/com/example/usuario/nikeapp/MainActivity.java
+++ app/src/main/java/com/example/usuario/nikeapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.usuario.nikeapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    LinearLayout btn01;
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -17,5 +22,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn01 = findViewById(R.id.btnEx01); btn01.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btnEx01:
+                Intent i = new Intent(this, ExerciseScreen.class);
+                startActivity(i);
+                break;
+        }
     }
 }
